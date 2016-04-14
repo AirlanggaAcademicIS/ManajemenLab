b7e4f7237ed129ce737738c49604d7b376e19ac6
rezaphalevi <fade2reza@gmail.com>
2016-04-14 15:24:27 +0700

    k]]\

---
+++ PROJECT/LokasiBarang/LokasiBarang.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Lokasi Barang</title>
+</head>
+
+<body>
+<p>&nbsp;</p>
+<form id="form1" name="form1" method="post" action="">
+  <table width="1077" height="362" border="1">
+    <tr>
+      <td colspan="7"><img src="WATERMARK.png" width="694" height="120" alt=""/></td>
+    </tr>
+    <tr>
+      <td width="108"><strong>Kategori</strong></td>
+      <td width="29">&nbsp;</td>
+      <td width="112"><strong>Kategori</strong></td>
+      <td colspan="2"><strong>Komputer</strong></td>
+      <td width="259">&nbsp;</td>
+      <td width="246">&nbsp;</td>
+    </tr>
+    <tr>
+      <td><input type="submit" name="komputer" id="komputer" value="Komputer" /></td>
+      <td>&nbsp;</td>
+      <td colspan="3">&nbsp;</td>
+      <td><input type="submit" name="insert2" id="insert2" value="Search" /></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><input type="submit" name="non-komputer" id="non-komputer" value="Non-Komputer" /></td>
+      <td>&nbsp;</td>
+      <td><strong>ID-Barang</strong></td>
+      <td colspan="2" rowspan="2"><input name="merk2" type="text" id="merk2" value="ID-Barang" />        <input name="merk3" type="text" id="merk3" value="Nama Barang" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="2" rowspan="8">&nbsp;</td>
+      <td height="30"><strong>Nama Barang</strong></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><strong>Keterangan</strong></td>
+      <td colspan="2"><pre><textarea name="keterangan" cols="45" rows="5" wrap="soft" id="keterangan"></textarea>       
+     </pre></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><strong>Lokasi</strong></td>
+      <td colspan="2"><select name="select2" id="select2">
+      </select></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input name="insert" type="submit" id="insert" value="Insert" /></td>
+           <td><input type="button" name="save" id="save" value="Save" /></td>
+    </tr>
+  </table>
+  <p>&nbsp;</p>
+</form>
+<p>&nbsp;</p>
+</body>
+</html>
