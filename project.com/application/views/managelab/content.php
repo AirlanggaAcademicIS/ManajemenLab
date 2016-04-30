61f93d95098b2bf864500ce4c3846a7c140d35e3
Alia Firdauzy <alia.firdauzy@yahoo.com>
2016-04-30 14:53:36 +0700

    UI Kondisi Barang
    do : UI Kondisi Barang
    hambatan : belum ada
    to do : menyambungkan ke DB

---
+++ project.com/application/views/managelab/content.php
@@ -0,0 +1,158 @@
+<body>
+<div class="main">
+  <div class="header">
+    <br><br>
+  <a href="../home/index.html">
+  <img class="imgcenter" src="../home/images/logo.png"> </a></div>
+  
+  <div class="nav">
+  <ul class="menu">
+   <div class="dropdown">
+  <span><li><a href="../home/inventaris.html"><center>INVENTARIS</center></a></li></span>
+  <div class="dropdown-content">
+    <a href="../home/inventaris.html"><center>KOMPUTER</center></a>
+    <a href="../home/inventarisnonkomputer.html"><center>NON KOMPUTER</center></a>
+    
+  </div>
+  <span><li><a href="../home/ManageLab.html"><center>MANAGE LAB</center></a></li></span>
+  <div class="dropdown-content">
+    <a href="../home/Inventaris.html"><center>KONDISI BARANG</center></a><div class="dropdown-content">
+    	<a href="../home/inventaris.html"><center>KOMPUTER</center></a>
+    	<a href="../home/inventarisnonkomputer.html"><center>NON KOMPUTER</center></a>
+    
+    <a href="../home/inventarisnonkomputer.html"><center>LOKASI BARANG</center></a><div class="dropdown-content">
+    	<a href="../home/inventaris.html"><center>KOMPUTER</center></a>
+    	<a href="../home/inventarisnonkomputer.html"><center>NON KOMPUTER</center></a>
+  </div>
+
+</div>
+  <li><a href="../home/Penjadwalan.html"><center>PENJADWALAN</center></a></li>
+  <li><a href="../home/HistoriBarang.html"><center>HISTORI</center></a></li>
+  <li><a href="../home/ETicketing.html"><center>E-TICKETING</center></a></li>
+  </ul>
+  </div>
+  
+  <div class="content">
+    <div id="wrapper">
+  <div class="left"> </div>
+    <div class="right">
+      <blockquote>
+        <p class="pad"><br />
+          </p>
+      </blockquote>
+      <form id="form1" name="form1" method="post" action="">
+        <blockquote>
+          <blockquote>
+            <blockquote>
+<div id="content"></div>
+              <p class="style2"> <strong> SELAMAT DATANG, SELAMAT BEKERJA</strong></p>
+              <blockquote>
+                <p class="pad">Sistem ini memberikan kemudahan bagi petugas laboratorium komputer untuk memasukkan data barang. Dengan bahasa yang mudah dipahami, sistem ini sangat efektif dan efisien bagi petugas laboratorium komputer tanpa terbatas oleh tempat dan waktu. </p>
+              </blockquote>
+<blockquote>&nbsp;</blockquote>
+            </blockquote>
+          </blockquote>
+        </blockquote>
+        <table width="707" height="89" border="0">
+            <tr>
+              <th colspan="3" scope="row"><p>&nbsp;</p>
+              <p>INPUT KONDISI BARANG NON-KOMPUTER </p></th>
+            </tr>
+            <tr>
+              <th align="left" scope="row">ID Barang</th>
+              <td><select name="jenisBarang" id="jenisBarang">
+                <option value="NK001">NK001</option>
+                <option value="NK002">NK002</option>
+                <option value="NK003">NK003</option>
+                <option value="NK004">NK004</option>
+              </select></td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <th width="142" align="left" scope="row">Nama Barang</th>
+              <td width="287"><input type="text" name="namabarang" id="namabarang"></td>
+              <td width="264">&nbsp;</td>
+            </tr>
+            <tr>
+              <th align="left" scope="row">Status Barang</th>
+              <td><input type="radio" name="radio" id="barangbaik" value="barangbaik">
+              <label for="barangbaik">Baik</label></td>
+              <td><input type="radio" name="radio" id="barangburuk" value="barangburuk">
+              <label for="barangburuk">Buruk</label></td>
+            </tr>
+            <tr>
+              <th align="left" scope="row">Keterangan</th>
+              <td><textarea name="deskripsi" id="deskripsi" cols="45" rows="5"></textarea></td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <th align="left" scope="row">&nbsp;</th>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <th align="left" scope="row">&nbsp;</th>
+              <td><input type="submit" name="insert" id="insert" value="Insert" /></td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <th align="left" scope="row">&nbsp;</th>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+          </table>
+        <p>&nbsp;</p>
+        <table width="704" height="40" border="1" id="tampilan">
+          <tr>
+            <th align="center" bgcolor="#999999" scope="row"><strong>ID Barang</strong></th>
+            <td align="center" bgcolor="#999999"><strong>Jenis Barang</strong></td>
+            <td align="center" bgcolor="#999999"><strong>Merk</strong></td>
+            <td align="center" bgcolor="#999999"><strong>Jumlah</strong></td>
+            <td align="center" bgcolor="#999999"><strong>Deskripsi</strong></td>
+            </tr>
+          <tr>
+            <th align="center" scope="row">&nbsp;</th>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            </tr>
+          <tr>
+            <th align="center" scope="row">&nbsp;</th>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            </tr>
+          <tr>
+            <th align="center" scope="row">&nbsp;</th>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            </tr>
+          <tr>
+            <th align="center" scope="row">&nbsp;</th>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            </tr>
+          <tr>
+            <th align="center" scope="row">&nbsp;</th>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center">&nbsp;</td>
+            </tr>
+          </table>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+          
+          
+        </form>
+      <p class="pad"><br />
+      </p>
+  </div>
+  </div>
+  </div>
