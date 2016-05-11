a252fb9a4738c1ec42c54dccb06f31db446087f4
rezaphalevi <fade2reza@gmail.com>
2016-05-11 23:31:03 +0700

    MANAJEMEN LAB
    DO : membuat modul manajemen lab
    TO DO : membuat scenario lainnya, dan integerasi dengan UI utama
    Hambatan: Masih bingung proses CRUD pada model MVC dengan Codeigniter

---
+++ manajemenlab/application/views/element/v_footer.php
@@ -0,0 +1,15 @@
+<hr>
+        <div class="footer">
+            <p>&copy; Created By : <a href="mOH.rEZA pAHLEVI 081311633063" target="_blank"><strong>MANIVLAB.COM</strong></a></p>
+        </div>
+
+<script type="text/javascript" src="<?php echo base_url('asset/js/jquery.printPage.js')?>"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".btnPrint").printPage();
+    })
+</script>
+
+    </div>
+</body>
+
