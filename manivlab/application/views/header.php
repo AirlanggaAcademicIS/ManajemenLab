7e75207c601eefabc0fa6fa6a7791f1ac6c48acf
aisyahsa <aisyahsa.asa@gmail.com>
2016-05-14 13:51:15 +0700

    insert jquery di header
    do: input jquery di header
    todo: masih harus sambung-sambung js dengan view dll
    hambatan: prjalanan masih panjang

--- manivlab/application/views/header.php
+++ manivlab/application/views/header.php
@@ -10,5 +10,13 @@
     <link rel="stylesheet" href="<?php echo base_url();?>public/bower_components/bootstrap/dist/css/bootstrap.min.css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="<?php echo base_url();?>public/css/styles.css">
+    
+    
+    <!-- tambahan java script jquery -->
+   <script src="../assets/js/plugins/jquery-1.9.1.min.js"></script>
+   <script src="../assets/js/plugins/bootstrap.min.js"></script>
+	
+
       
   </head>
+
