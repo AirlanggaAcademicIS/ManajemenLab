51b948a5d458543c44c11d6896da02ce544e0479
ayurosalina <ayurosalinawibowo@gmail.com>
2016-05-15 21:21:04 +0700

    Scenario 2 Histo NonKomputer. Bismillah InsyaAllah udh jalan :')
    do : mebuat MVC Histori non-komputer dan menambahkan image dipublic
    to do : MVC Histori komputer
    hambatan : masih blm ada kolom tanggal di db aslinya

--- manivlab/application/controllers/History.php
+++ manivlab/application/controllers/History.php
@@ -13,26 +13,79 @@ class History extends CI_Controller
  
  public function index()
 	{
-		
-    # code...
-    $this->load->model('model');
+		 # code...
+ /*   $this->load->model('model');
 	$tabel=$this->model->Getpppk();
-	$a = array (
-		'table'=> $tabel
-	);
+	$a = array ('table'=> $tabel);
+	 $tabel2=$this->model->GetIden();
+	$aa = array ('table2'=> $tabel2); */
 	
+		$this->load->model('model');
+		$data['table']=$this->model->Getpppk();
+		$data['table2']=$this->model->GetIden();
+		
+		$this->load->model('M_nonkomp');
+		$data['nonkomp']=$this->M_nonkomp->gettablenonkomp();
+		
+
+		$this->load->model('Manage_lab');
+		$data['manajemenbarang']=$this->Manage_lab->gettablemanajemen_barang();
+		
+
 	$this->load->view('header');
-    $this->load->view('history/content',$a);
+    $this->load->view("history/content",$data);
     $this->load->view('footer');
 	
-	
-	
-	
+	} 
+	function viewkomputer(){
+		$this->load->model('model');
+		$data['table']=$this->model->Getpppk();
+		$data['table2']=$this->model->GetIden();
+		
+		$this->load->model('inventaris_komputer');
+		$data['komp']=$this->inventaris_komputer->gettablekomputer();
+		
+
+		$this->load->model('Manage_lab');
+		$data['manajemenbarang']=$this->Manage_lab->gettablemanajemen_barang();
+		
+
+	$this->load->view('header');
+    $this->load->view("history/viewkomputer",$data);
+    $this->load->view('footer');
 	}
-
 	
+	function viewhistorylaporan(){
+	$this->load->view('header');
+    $this->load->view("history/viewhistorylaporan");
+    $this->load->view('footer');
+	}
+	
+	// Mengambil laporan history
+	function getLaporanItemByTgl(){
+		$start_date = $this->input->post("start_date");
+		$end_date = $this->input->post("end_date");
+		$start_date = date('Y-m-d',strtotime($start_date));
+		$end_date = date('Y-m-d',strtotime($end_date));
+		$kategori = $this->input->post("kategori");
+		$this->load->model('model');
+		$result['item_pppk'] = $this->model->GetpppkbyTanggal($start_date,$end_date,$kategori);
+		$result['iden'] = $this->model->GetIdenbyTanggal($start_date,$end_date,$kategori);
 
- 
+		// print_r($result['item_masuk']);
+		// print_r($result['item_keluar']);
+		echo json_encode($result);
+	}
+	
+/*	public function histononkomp()
+	{ 
+	
+	$this->load->view("history/content",$data);
 
 
+
+	$this->load->view('header');
+    
+    $this->load->view('footer');
+	}*/
 }
