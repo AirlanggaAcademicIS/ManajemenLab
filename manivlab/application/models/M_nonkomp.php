fc3c72feea35aa623ba5bcaa8afbf19626e3b50b
Emilia F.F <emilia.fitria@gmail.com>
2016-05-18 07:36:11 +0700

    Bismillah re-upload tampil data nonkomp dari edit ke dalam field dan memunculkan di tabel kembali
    Do : menampilkan data nonokomputer dg button edit dari tabel field lalu
    di lakukan update data pada field dan muncul ditabel
    To Do : menampilkan search ID dan tampil nama petugas lab
    Hambatan : tidak dapat sync karena branch

--- manivlab/application/models/M_nonkomp.php
+++ manivlab/application/models/M_nonkomp.php
@@ -7,15 +7,16 @@ class M_nonkomp extends CI_Model {
         parent::__construct();
     }
 
-    public function insertnonkomp($param1, $param2,$param3,$param4,$param5,$param6) {
+    public function insertnonkomp($param1,$param2,$param3,$param4,$param5,$param6,$param7) {
         $data = array(
         'id_nonkomp' => null,
         'nama_nonkomp'=>$param1,
-        'Jenis_nonkomp'=>$param2,
-        'merk_nonkomp'=>$param3,
-        'jumlah_nonkomp'=>$param4,
-        'lokasi_nonkomp'=>$param5,
-        'keterangan_nonkomp'=>$param6,     
+        'tanggal_transaksi'=>$param2,
+        'Jenis_nonkomp'=>$param3,
+        'merk_nonkomp'=>$param4,
+        'jumlah_nonkomp'=>$param5,
+        'lokasi_nonkomp'=>$param6,
+        'keterangan_nonkomp'=>$param7,
         );
 
      $this->db->insert('nonkomputer',$data);
@@ -26,6 +27,29 @@ class M_nonkomp extends CI_Model {
         
     return $this->db->get('nonkomputer');
     
+    }
+    public function getdata($id){
+        $query = $this->db->get_where('nonkomputer',array('id_nonkomp'=>$id));
+        return $query->result();
+    }
+    public function updatedata($id,$namabarang,$tanggal_transaksi,$jenisbarang,$merkbarang,$jumlahbarang,$lokasibarang,$keteranganbarang){
+        $this->db->trans_start();
+        $data = array ('nama_nonkomp'=>$namabarang,
+                      'tanggal_transaksi'=>$tanggal_transaksi,
+                      'Jenis_nonkomp'=>$jenisbarang,
+                      'merk_nonkomp'=>$merkbarang,
+                      'jumlah_nonkomp'=>$jumlahbarang,
+                      'lokasi_nonkomp'=>$lokasibarang,
+                      'keterangan_nonkomp'=>$keteranganbarang);
+        $this->db->where('id_nonkomp',$id);
+        $this->db->update("nonkomputer",$data);
+        $this->db->trans_complete();
+        if($this->db->affected_rows()>1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     
 }
---
+++ manivlab/public/js/edit.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    console.log("test");
+  $(".edit").click(function(){
+    console.log(this.value);
+      $.ajax({
+          url:"http://localhost/manivlab/inventaris/getdatanonkomp",
+          dataType:"json",
+          type:"post",
+          data:{"id":this.value}
+      }).done(function(data){
+          console.log(data);
+          $.each(data,function(index,value){
+              $("#namabarang").val(value.nama_nonkomp);
+         $("#tanggal_transaksi").val(value.tanggal_transaksi);
+         $("#jenisbarang").val(value.Jenis_nonkomp);
+         $("#merkbarang").val(value.merk_nonkomp);
+         $("#jumlahbarang").val(value.jumlah_nonkomp);
+         $("#lokasibarang").val(value.lokasi_nonkomp);
+         $("#keterangan").val(value.keterangan_nonkomp);
+         $("#updatedata").attr("data-id",value.id_nonkomp);
+          });
+         
+      });
+    });
+    
+    $("#updatedata").click(function(){
+         var nama_barang = $("#namabarang").val();
+         var tanggal_transaksi = $("#tanggal_transaksi").val();
+         var jenis_barang = $("#jenisbarang").val();
+         var merk_barang = $("#merkbarang").val();
+         var jumlah_barang = $("#jumlahbarang").val();
+         var lokasi_barang = $("#lokasibarang").val();
+         var keterangan = $("#keterangan").val();
+         var id = $("#updatedata").attr("data-id");
+         console.log(id);
+        if(nama_barang !="" && tanggal_transaksi!="",jenis_barang!="",merk_barang!="",jumlah_barang!="",lokasi_barang!="",keterangan!=""){
+            $.ajax({
+                url:"http://localhost/manivlab/inventaris/updatenonkomp",
+                dataType:"json",
+                type:"POST",
+                data:{"id":id,"namabarang":nama_barang,"tanggal_transaksi":tanggal_transaksi,"jenisbarang":jenis_barang,"merkbarang":merk_barang,"jumlahbarang":jumlah_barang,"lokasibarang":lokasi_barang,"keteranganbarang":keterangan}
+            }).done(function(data){
+                if(data){
+                    console.log("berhasil");
+                    location.reload();
+                }
+                else{
+                    console.log('gagal');
+                    location.reload();
+                }
+            });
+        }
+    });
+})
